fd67e0c3fb0eb28fb9d93582a39cf6979205f80c
Yasu <xxx@test.com>
2018-02-03 15:21:52 +0900

    first commit
    first commit

---
+++ src/main/java/jp/takehara/Main.java
@@ -0,0 +1,10 @@
+package jp.takehara;
+
+
+
+public class Main {
+    public static void main(String[] args){
+        MessageSender messageSender = new MessageSender();
+        messageSender.send();
+    }
+}
